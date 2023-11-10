f1626a45fc371f68e34d7aa0d738a0671633f8f2
visuallisation <florentin.rieger@gmail.com>
2023-11-10 14:38:39 +0100

    feat(8086): get registers

--- 8086/8086.cpp
+++ 8086/8086.cpp
@@ -11,13 +11,37 @@ std::map<unsigned char, std::string> operations {
     { 34, "mov" }
 };
 
+// assembly register table
+std::map<unsigned char, std::string> registers {
+    // 0 = 000 = ax
+    { 0, "ax" },
+    // 1 = 001 = cx
+    { 1, "cx" },
+    // 2 = 010 = dx
+    { 2, "dx" },
+    // 3 = 011 = bx
+    { 3, "bx" },
+};
+
 // the operation is stored in the upper 6 bits of the first byte: 11111100 = 252
 unsigned char operationMask = 252;
 // since the operation is stored in the upper 6 bits we have to shift byte 2 bits to the right to get the value
 unsigned char operationShift = 2;
 
-std::string getOperation(unsigned char operation) {
-    return operations[operation] == "" ? "unknown operation" : operations[operation];
+// the source is stored in the lower 3 bits of the second byte: 00000111 = 7
+// since the source is stored in the lower 3 bits we don't have to shift the byte
+unsigned char sourceMask = 7;
+// the destination is stored in the middle 3 bits of the second byte: 00111000 = 56
+unsigned char destinationMask = 56;
+// since the destination is stored in the middle 3 bits we have to shift byte 3 bits to the right to get the value
+unsigned char destinationShift = 3;
+
+std::string getOperation(unsigned char code) {
+    return operations[code] == "" ? "unknown operation" : operations[code];
+}
+
+std::string getRegister(unsigned char code) {
+    return registers[code] == "" ? "unknown register" : registers[code];
 }
 
 int main() {
@@ -26,6 +50,8 @@ int main() {
     std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(input), {});
     
     unsigned char operation = (buffer[0] & operationMask) >> operationShift;
-    std::cout << getOperation(operation) << std::endl;
+    unsigned char source = buffer[1] & sourceMask;
+    unsigned char destination = (buffer[1] & destinationMask) >> destinationShift;
+    std::cout << getOperation(operation) << " " << getRegister(source) << ", " << getRegister(destination) << std::endl;
 }
 
