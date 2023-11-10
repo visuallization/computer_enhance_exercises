36e7278388aca7947b8d95715132050d3d4b2ac9
visuallisation <florentin.rieger@gmail.com>
2023-11-10 14:52:43 +0100

    feat(8086): also consider the direction bit

--- 8086/8086.cpp
+++ 8086/8086.cpp
@@ -28,9 +28,15 @@ unsigned char operationMask = 252;
 // since the operation is stored in the upper 6 bits we have to shift byte 2 bits to the right to get the value
 unsigned char operationShift = 2;
 
+// the direction is stored in the second bit of the first byte: 00000010 = 2
+unsigned char directionMask = 2;
+// since the direction is stored in the second bit we have to shift byte 1 bit to the right to get the value
+unsigned char directionShift = 1;
+
 // the source is stored in the lower 3 bits of the second byte: 00000111 = 7
-// since the source is stored in the lower 3 bits we don't have to shift the byte
 unsigned char sourceMask = 7;
+// since the source is stored in the lower 3 bits we don't have to shift the byte
+unsigned char sourceShift = 0;
 // the destination is stored in the middle 3 bits of the second byte: 00111000 = 56
 unsigned char destinationMask = 56;
 // since the destination is stored in the middle 3 bits we have to shift byte 3 bits to the right to get the value
@@ -50,8 +56,15 @@ int main() {
     std::vector<unsigned char> buffer(std::istreambuf_iterator<char>(input), {});
     
     unsigned char operation = (buffer[0] & operationMask) >> operationShift;
-    unsigned char source = buffer[1] & sourceMask;
-    unsigned char destination = (buffer[1] & destinationMask) >> destinationShift;
+    unsigned char direction = (buffer[0] & directionMask) >> directionShift;
+
+    unsigned char source = direction 
+        ? (buffer[1] & destinationMask) >> destinationShift 
+        : (buffer[1] & sourceMask) >> sourceShift;
+    unsigned char destination = direction 
+        ? (buffer[1] & sourceMask) << sourceShift 
+        : (buffer[1] & destinationMask) >> destinationShift;
+
     std::cout << getOperation(operation) << " " << getRegister(source) << ", " << getRegister(destination) << std::endl;
 }
 
